53547bbe29c5b1e5f328f31bedbe18c3b4864661
louishrg <vundaboy@gmail.com>
2017-11-14 23:37:06 +0100

    Updating docker, it works !

--- config/conf.inc.php
+++ config/conf.inc.php
@@ -1,13 +1,13 @@
 <?php
 // Windows
-// define("DBHOST", "192.168.99.100"); 
+define("DBHOST", "192.168.99.100"); 
 
 // Linux
-define("DBHOST", "localhost");
+// define("DBHOST", "localhost");
 
 define("DBPORT", "3306");
-define("DBUSER", "63a9f0ea7bb98050796b649e85481845");
-define("DBPWD", "a029d0df84eb5549c641e04a9ef389e5");
+define("DBUSER", "root");
+define("DBPWD", "root");
 define("DBNAME", "wesic");
 define('PATH_ROOT', ""); // chemin vers le site sur le 
 define('PATH_FILE', "");
--- ham/js/index.js
+++
@@ -1,5 +0,0 @@
-var wrapperMenu = document.querySelector('.wrapper-menu');
-
-wrapperMenu.addEventListener('click', function(){
-  wrapperMenu.classList.toggle('open');  
-})
