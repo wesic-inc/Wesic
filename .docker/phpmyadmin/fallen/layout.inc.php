92d0431f63f9d86a1b3162b345cf2ca7102f219c
louishrg <vundaboy@gmail.com>
2017-11-14 11:58:07 +0100

    Mysql persist fix & PhpMyAdmin theming

---
+++ .docker/phpmyadmin/fallen/layout.inc.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Layout
+ *
+ * @package Fallen
+ * @author  Frans Allen
+ * @link    https://www.fransallen.com
+ */
+
+/*
+ * General
+ */
+$GLOBALS['cfg']['NaviWidth']                = '250';
+$GLOBALS['cfg']['FontFamily']               = '"Roboto", Helvetica, Arial, sans-serif';
+$GLOBALS['cfg']['FontFamilyLight']          = '"Roboto", "Segoe UI Light", "Segoe UI"; font-weight: 300';
+$GLOBALS['cfg']['FontFamilyFixed']          = 'Consolas, Monospace, "Lucida Grande"';
+
+/*
+ * Color Scheme
+ * No color scheme for this version
+ */
+$GLOBALS['cfg']['BrowsePointerColor']       = '#6c7a89';
+$GLOBALS['cfg']['BrowseMarkerColor']        = '#000';
+$GLOBALS['cfg']['BrowseWarningColor']       = '#ea4335';
+$GLOBALS['cfg']['BrowseSuccessColor']       = '#01a31c';
+$GLOBALS['cfg']['BrowseGrayColor']          = '#ccc';
+$GLOBALS['cfg']['BrowseMarkerBackground']   = '#eee';
+$GLOBALS['cfg']['BorderColor']              = '#ddd';
+$GLOBALS['cfg']['ButtonColor']              = '#fff';
+$GLOBALS['cfg']['ButtonBackground']         = '#6c7a89';
+$GLOBALS['cfg']['ButtonHover']              = '#b8bfc7';
+$GLOBALS['cfg']['BgOne']                    = '#f7f7f7';
+$GLOBALS['cfg']['BgTwo']                    = '#fff';
+$GLOBALS['cfg']['BlueHeader']               = '#6c7a89';
+$GLOBALS['cfg']['NaviColor']                = '#eee';
+$GLOBALS['cfg']['NaviBackground']           = '#6c7a89';
+$GLOBALS['cfg']['NaviBackgroundLight']      = '#b8bfc7';
+$GLOBALS['cfg']['NaviPointerColor']         = '#333';
+$GLOBALS['cfg']['NaviPointerBackground']    = '#6c7a89';
+$GLOBALS['cfg']['NaviDatabaseNameColor']    = '#333';
+$GLOBALS['cfg']['NaviHoverBackground']      = '#b8bfc7';
+$GLOBALS['cfg']['MainColor']                = '#444';
+$GLOBALS['cfg']['MainBackground']           = '#fff';
+$GLOBALS['cfg']['ThBackground']             = '#f7f7f7';
+$GLOBALS['cfg']['ThDisabledBackground']     = '#f3f3f3';
+$GLOBALS['cfg']['ThColor']                  = '#666';
+$GLOBALS['cfg']['ThPointerColor']           = '#000';
--- config/conf.inc.php
+++ config/conf.inc.php
@@ -1,13 +1,13 @@
 <?php
 // Windows
-define("DBHOST", "192.168.99.100"); 
+// define("DBHOST", "192.168.99.100"); 
 
 // Linux
-// define("DBHOST", "127.0.0.1");
+define("DBHOST", "localhost");
 
 define("DBPORT", "3306");
-define("DBUSER", "root");
-define("DBPWD", "mypass");
+define("DBUSER", "63a9f0ea7bb98050796b649e85481845");
+define("DBPWD", "a029d0df84eb5549c641e04a9ef389e5");
 define("DBNAME", "wesic");
 define('PATH_ROOT', ""); // chemin vers le site sur le 
 define('PATH_FILE', "");
