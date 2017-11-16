a425d0309a2a9c0509d83c513d787ef8ba5181c8
louishrg <vundaboy@gmail.com>
2017-11-16 20:22:36 +0100

    Updating assets link

--- views/template/template.php
+++ views/template/template.php
@@ -2,22 +2,21 @@
 <html lang="fr">
 	<head>
 		<meta charset="UTF-8">
-		<title>Titre de ma page</title>
+		<title><?php if(isset($title)) echo $title ?></title>
 		<meta name="description" content="description de ma page">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
-		<link rel="icon" type="image/png" href="<?php echo routing::getRoot() ?>assets/img/favicon.png" />
+		<link rel="icon" type="image/png" href="/assets/img/favicon.png" />
 
-		<link rel="stylesheet" type="text/css" href="<?php echo routing::getRoot() ?>assets/css/core.css">
-		<link rel="stylesheet" type="text/css" href="<?php echo routing::getRoot() ?>assets/css/grid.css">
-		<link rel="stylesheet" type="text/css" href="<?php echo routing::getRoot() ?>assets/css/style.css">
-		<link rel="stylesheet" type="text/css" href="<?php echo routing::getRoot() ?>assets/css/theme.css">
-		<title><?php if(isset($title)) echo $title ?></title>
+		<link rel="stylesheet" type="text/css" href="/assets/css/core.css">
+		<link rel="stylesheet" type="text/css" href="/assets/css/grid.css">
+		<link rel="stylesheet" type="text/css" href="/assets/css/style.css">
+		<link rel="stylesheet" type="text/css" href="/assets/css/theme.css">
 	</head>
 	<body>
 
 		<?php include $this->view;?>	
-		<script type="text/javascript" src="<?php echo routing::getRoot() ?>assets/js/jquery.min.js"></script>
-		<script type="text/javascript" src="<?php echo routing::getRoot() ?>assets/js/code.js"></script>
+		<script type="text/javascript" src="/assets/js/jquery.min.js"></script>
+		<script type="text/javascript" src="/assets/js/code.js"></script>
 	</body>
 </html>
