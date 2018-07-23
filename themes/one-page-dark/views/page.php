93a0ec54066c3db3c9435600f7ae6a3993e536e2
pierretruchot <pierre.truchot2@gmail.com>
2018-07-23 22:12:38 +0200

    adding one pagr dark && starting media first template

---
+++ themes/one-page-dark/views/page.php
@@ -0,0 +1,10 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<?php page_featured(); ?>
+			<?php page_title(); ?>
+			<?php page_date(); ?>
+			<?php page_content(); ?>
+		</div>
+	</div>
+</div>
